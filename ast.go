f59863ddfee8cc3d6d7d14f37016ad78b276fc55
OKAMOTO Shigehiro <primary@goahi.live>
2024-03-16 14:25:19 +0900

    implement plus

--- ast.go
+++ ast.go
@@ -1,17 +1,64 @@
 package main
 
-type ConstantVal struct {
-	val int
+type ExprType int
+
+const (
+	Constant ExprType = iota
+	Plus     ExprType = iota
+)
+
+type Exprs struct {
+	PlusOp      *PlusOp
+	ConstantVal *ConstantVal
 }
 
-func newConstantVal(val int) *ConstantVal{
-	cv := new(ConstantVal)
-	cv.val = val
-	return cv
+type Expr struct {
+	Type     ExprType
+	Operator *Exprs
 }
 
-func (cv *ConstantVal)get() int {
-	return cv.val
+func newExpr(t ExprType, e *Exprs) *Expr {
+	ex := new(Expr)
+	ex.Type = t
+	ex.Operator = e
+	return ex
+
 }
 
+func (e *Expr) eval() int {
+	switch e.Type {
+	case Constant:
+		return e.Operator.ConstantVal.eval()
+	case Plus:
+		return e.Operator.PlusOp.eval()
+	default:
+		panic("Invalid operator")
+	}
+}
 
+type ConstantVal int
+
+func newConstantVal(val int) *ConstantVal {
+	cv := ConstantVal(val)
+	return &cv
+}
+
+func (cv *ConstantVal) eval() int {
+	return int(*cv)
+}
+
+type PlusOp struct {
+	LeftExpr  *Expr
+	RightExpr *Expr
+}
+
+func newPlusOp(leftExpr *Expr, rightExpr *Expr) *PlusOp {
+	po := new(PlusOp)
+	po.LeftExpr = leftExpr
+	po.RightExpr = rightExpr
+	return po
+}
+
+func (po *PlusOp) eval() int {
+	return po.LeftExpr.eval() + po.RightExpr.eval()
+}
--- ast_test.go
+++ ast_test.go
@@ -1,15 +1,32 @@
 package main
 
 import (
-	"testing"
 	"reflect"
+	"testing"
 )
 
-func TestConstantVal(t *testing.T){
+func TestConstantVal(t *testing.T) {
 	expect := 55
 	constantVal := newConstantVal(expect)
-	
-	if !reflect.DeepEqual(expect, constantVal.get()){
-		t.Fatal("Test Failed!")
+
+	if !reflect.DeepEqual(expect, constantVal.eval()) {
+		t.Fail()
+	}
+}
+
+func TestPlusOp(t *testing.T) {
+	// 13+(5+1)
+	thirteen := newExpr(Constant, &Exprs{ConstantVal: newConstantVal(13)})
+	five := newExpr(Constant, &Exprs{ConstantVal: newConstantVal(5)})
+	one := newExpr(Constant, &Exprs{ConstantVal: newConstantVal(1)})
+
+	fivePlusOne := newExpr(Plus, &Exprs{PlusOp: newPlusOp(five, one)})
+
+	plusOp := newExpr(Plus, &Exprs{PlusOp: newPlusOp(thirteen, fivePlusOne)})
+
+	expect := 13 + (5 + 1)
+
+	if !reflect.DeepEqual(plusOp.eval(), expect) {
+		t.Fail()
 	}
 }
